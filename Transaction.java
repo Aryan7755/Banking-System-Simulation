ab2d832e344c4c6d4a279a18d0bf757f7203656c
Aryan raj <aryanbanthu@gmail.com>
2026-03-25 17:41:25 +0530

    Added the member variables of Transaction class.

--- Transaction.java
+++ Transaction.java
@@ -1,2 +1,10 @@
+import java.time.LocalDateTime;
+
 public class Transaction {
+    private static int counter = 1;
+
+    private int transactionId;
+    private String type;
+    private double amount;
+    private LocalDateTime date;
 }
