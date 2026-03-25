7b4714667dd4266cc35b79adce4c772545445071
Aryan raj <aryanbanthu@gmail.com>
2026-03-25 17:42:13 +0530

    Added the Constructor for Transaction class.

--- Transaction.java
+++ Transaction.java
@@ -7,4 +7,11 @@ public class Transaction {
     private String type;
     private double amount;
     private LocalDateTime date;
+
+    public Transaction(String type, double amount) {
+        this.transactionId = counter++;
+        this.type = type;
+        this.amount = amount;
+        this.date = LocalDateTime.now();
+    }
 }
