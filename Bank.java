9fe4abc40c1e98631ba8bb8f48bd7c7d9001e05d
Aryan raj <aryanbanthu@gmail.com>
2026-03-25 17:50:44 +0530

    Added the createAccount method in the Bank class.

--- Bank.java
+++ Bank.java
@@ -3,4 +3,12 @@ import java.util.*;
 public class Bank {
     private Map<Integer, Account> accounts = new HashMap<>();
     private static int accountCounter = 1001;
+
+    public void createAccount(String name) {
+        int accNo = accountCounter++;
+        Account acc = new Account(accNo, name);
+        accounts.put(accNo, acc);
+
+        System.out.println("Account created. Account Number: " + accNo);
+    }
 }
