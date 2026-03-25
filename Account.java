62ee2c2f376f1a3738a095ffdf55a238b9734192
Aryan raj <aryanbanthu@gmail.com>
2026-03-25 17:46:07 +0530

    Added the Constructor for the Account class.

--- Account.java
+++ Account.java
@@ -1,6 +1,15 @@
+import java.util.*;
+
 public class Account {
     private int accountNumber;
     private String holderName;
     private double balance;
     private List<Transaction> transactions;
+
+    public Account(int accountNumber, String holderName) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = 0;
+        this.transactions = new ArrayList<>();
+    }
 }
