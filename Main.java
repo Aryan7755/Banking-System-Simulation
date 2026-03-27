f6d05c002a39589d4d0d0e6af8fa5b871f237d5e
Aryan raj <aryanbanthu@gmail.com>
2026-03-27 16:25:26 +0530

    Added Menu-driven system (CLI) using main.

--- Main.java
+++ Main.java
@@ -1,14 +1,60 @@
 import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Bank bank = new Bank();
 
+        bank.loadFromFile();
 
+        while (true) {
+            System.out.println("\n--- Banking System ---");
+            System.out.println("1. Create Account");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Transaction History");
+            System.out.println("5. Exit");
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            Bank bank = new Bank();
+            int choice = sc.nextInt();
 
-            bank.createAccount("Aryan");
-            bank.depositAmount(1001, 5000);
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter name: ");
+                    sc.nextLine();
+                    String name = sc.nextLine();
+                    bank.createAccount(name);
+                    break;
+
+                case 2:
+                    System.out.print("Enter account number: ");
+                    int accNo = sc.nextInt();
+                    System.out.print("Enter amount: ");
+                    double amt = sc.nextDouble();
+                    bank.depositAmount(accNo, amt);
+                    break;
+
+                case 3:
+                    System.out.print("Enter account number: ");
+                    accNo = sc.nextInt();
+                    System.out.print("Enter amount: ");
+                    amt = sc.nextDouble();
+                    bank.withdrawAmount(accNo, amt);
+                    break;
+
+                case 4:
+                    System.out.print("Enter account number: ");
+                    accNo = sc.nextInt();
+                    bank.showHistory(accNo);
+                    break;
+
+                case 5:
+                    bank.saveToFile();
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice");
+            }
         }
-
+    }
 }
